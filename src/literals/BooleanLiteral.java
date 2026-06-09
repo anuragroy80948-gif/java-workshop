bd7d4c5589533175bbd446d85c777f0594221da3
anuragroy80948-gif <anuragroy80948@gmail.com>
2026-06-09 15:08:20 +0530

    Add files via upload

---
+++ src/literals/BooleanLiteral.java
@@ -0,0 +1,14 @@
+package literals;
+
+public class BooleanLiteral {
+	public static void main(String[] args) {
+		boolean a=true;
+		boolean b=false;
+
+		System.out.println(a);
+		System.out.println(b);
+		
+		
+
+	}
+}
---
+++ src/operatorAssignment/AbsoluteValue.java
@@ -0,0 +1,16 @@
+package operatorAssignment;
+
+public class AbsoluteValue {
+	public static void main(String[] args) {
+		java.util.Scanner sc = new java.util.Scanner(System.in);
+
+		System.out.println("Enter number:");
+		int n = sc.nextInt();
+
+		int abs = (n < 0) ? -n : n;
+
+		System.out.println("Absolute Value: " + abs);
+
+		sc.close();
+	}
+}
